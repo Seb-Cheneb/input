4bd2035d996c1405bb5fab4bef2e5cfcef903734
Seb-Cheneb <bucovala.sebastian@protonmail.com>
2025-06-17 22:38:09 +0300

    Update submodule: scripts/input

--- input_controller.gd
+++ input_controller.gd
@@ -11,10 +11,23 @@ var input_direction: Vector2 = Vector2.ZERO
 var input_vector: Vector3 = Vector3.ZERO
 
 
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
 func _physics_process(delta: float) -> void:
 	get_input_vector()
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+## gets the ground movement input direction and updates the input vector
 func get_input_vector():
 	current_input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	if not current_input_direction.is_equal_approx(input_direction):
