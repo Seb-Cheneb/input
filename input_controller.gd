27dc3d2cad124b6130b65195e481d4a5745b4d86
Seb-Cheneb <bucovala.sebastian@protonmail.com>
2025-06-17 21:26:40 +0300

    Update submodule: scripts/input

--- input_controller.gd
+++ input_controller.gd
@@ -1,20 +1,41 @@
 @icon("./input.png")
-class_name InputController extends Node
+class_name InputController 
+extends Node
 
 
-@export var _actor: Node3D
-var _action_list: Dictionary = {}
+@export_category("Debugging")
+@export var is_debugging: bool = false
+
+var current_input_direction: Vector2 = Vector2.ZERO
+var input_direction: Vector2 = Vector2.ZERO
+var input_vector: Vector3 = Vector3.ZERO
 
 
-func _ready() -> void:
-	_action_list = {
-		"left": MoveLeftCommand.new(_actor)
-	}
+func _physics_process(delta: float) -> void:
+	get_input_vector()
 
 
-func _unhandled_input(event: InputEvent) -> void:
-	if not event is InputEventAction:
-		return
+func get_input_vector():
+	current_input_direction = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	if not current_input_direction.is_equal_approx(input_direction):
+		input_direction = current_input_direction
+		input_vector = Vector3(input_direction.x, 0, input_direction.y).normalized()
+		Logger.info(is_debugging, self, "new input vector detected: " + str(input_vector))
 
-	if event.pressed && event.action in _action_list:
-			_action_list[event.action].execute()
+
+#@export var _actor: Node3D
+#var _action_list: Dictionary = {}
+
+
+# func _ready() -> void:
+# 	# _action_list = {
+# 	# 	"left": MoveLeftCommand.new(_actor)
+# 	# }
+
+
+#func _unhandled_input(event: InputEvent) -> void:
+	#if not event is InputEventAction:
+		#return
+#
+	#if event.pressed && event.action in _action_list:
+			#_action_list[event.action].execute()
